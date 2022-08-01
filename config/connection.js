c536bfc7255e7bab3bf8d7c5118aad05fb2dab5a
ian.moss <ianmsieg@gmail.com>
2022-08-01 15:57:42 -0500

    added config folder and connection, began building mvc folder structure

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequilize = require('sequelize')
+require('dotenv').config()
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequilize(process.env.JAWSDB_URL)
+} else {
+    sequelize = new Sequilize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        }
+    )
+}
+
+module.exports=sequelize
