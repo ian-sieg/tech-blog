51bd0560d0fda7dfbf122dd1d1f63f29da206f10
ian.moss <ianmsieg@gmail.com>
2022-08-02 12:13:34 -0500

    created seed data and seed.js file

---
+++ seeds/seed.js
@@ -0,0 +1,33 @@
+const sequilize = require('../config/connection')
+const {User, Post, Comment} = require('../models')
+
+const userData = require('./userData.json')
+const postData = require('./postData.json')
+const commentData = require ('./commentData.json')
+
+const seedDatabase = async () => {
+    await sequilize.sync({force: true});
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const post of postData) {
+        await Post.create({
+            ...post,
+            user_id: users[Math.floor(Math.random() * users.length)].id
+        });
+    };
+
+    for (const comment of commentData) {
+        await Comment.create({
+            ...comment,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
+            post_id: postData[Math.floor(Math.random() * postData.length)].id
+        });
+    };
+    process.exit(0)
+};
+
+seedDatabase();
