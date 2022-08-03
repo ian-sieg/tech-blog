cb1e85d44892fdbbce10d0933f9875cc26f028be
ian.moss <ianmsieg@gmail.com>
2022-08-03 11:55:40 -0500

    updated main template handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,18 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
+        const postData = await Post.findAll({
+            include: [{
+                model: User,
+                attributes: ['username']
+            }],
+            order: [
+                ['updated_at', 'ASC']
+            ]
+        })
+        const posts = postData.map((post) => post.get({plain:true}))
         res.render('homepage', {
+            posts,
             logged_in: req.session.logged_in
         });
     } catch (err) {
